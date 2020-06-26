8f75cd12d7cfd5607385764eaff29b79134a8b6e
Hétcor <hector_ibi_94@hotmail.com>
2020-06-26 16:20:33 +0200

    Añadir primeros ficheros

--- Conexion/Conexion.java
+++ Conexion/Conexion.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package Conexion;
 
 import Usuario.Usuario;
@@ -54,3 +55,61 @@ public class Conexion {
             
     }
 }
+=======
+package Conexion;
+
+import Usuario.Usuario;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Conexion {
+    private Connection getConnection() throws SQLException {
+       
+        String url = "jdbc:mysql://localhost:3306/proyectoeoi?useSSL=false&serverTimezone=UTC";
+	String user = "root";
+	String pass = "CyanDolphins55";
+	
+        try{
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de MySQL: " + ex);
+            }
+        
+	return DriverManager.getConnection(url, user, pass);
+	}
+    
+    public boolean createUsuario(Usuario usuario) {
+	String insertQuery = "INSERT INTO usuarios(usuario, password, telefono móvil, telefono secundario,  email) VALUES (?, ?, ?, ?, ?)";
+	Connection con = null;
+	PreparedStatement stmt = null;
+	int rows = 0;
+            try {
+                con = getConnection();
+                stmt = con.prepareStatement(insertQuery);
+		stmt.setString(1, usuario.getUsuario());
+                stmt.setString(2, usuario.getPassword());
+                stmt.setInt(3, usuario.getTelefono());
+                stmt.setInt(4, usuario.getTelefono2());
+                stmt.setString(5, usuario.getEmail());
+                System.out.println("Ejecutando la query: " + insertQuery);
+				
+		rows = stmt.executeUpdate();
+                System.out.println("Registros afectados: " + rows);
+				
+		stmt.close();
+                con.close();
+				
+		return true;
+				
+				
+	    } catch (SQLException e) {
+		e.printStackTrace();
+		return false;
+        }
+        
+            
+    }
+}
+>>>>>>> Stashed changes
--- web/ServletProyectoEOI.java
+++ web/ServletProyectoEOI.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package web;
 
 import java.io.IOException;
@@ -13,3 +14,20 @@ public class ServletProyectoEOI extends HttpServlet {
          
      }
 }
+=======
+package web;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+    @WebServlet("/ServletProyectoEOI")
+public class ServletProyectoEOI extends HttpServlet {
+    @Override
+     protected void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException{
+         
+     }
+}
+>>>>>>> Stashed changes
